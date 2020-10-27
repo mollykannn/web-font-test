091fe459cbe97c58f066f2d6f48fcfcd15c05fba
mollykannn <mollykannn@gmail.com>
2020-10-27 13:13:31 +0000

    Deploying to gh-pages from  @ f10b888e43802eea6c4a4a211da0f7988b9b2584 🚀

---
+++ precache-manifest.572476a80183b3731931efb3260d7a29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5790151e0942f84529e6",
+    "url": "/web-font-test/css/app.17ff16f6.css"
+  },
+  {
+    "revision": "e435a8b8124e21decbfa",
+    "url": "/web-font-test/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "5a2c11d31f7d2f6a6e0b73e1c9c09157",
+    "url": "/web-font-test/index.html"
+  },
+  {
+    "revision": "5790151e0942f84529e6",
+    "url": "/web-font-test/js/app.58693cd3.js"
+  },
+  {
+    "revision": "e435a8b8124e21decbfa",
+    "url": "/web-font-test/js/chunk-vendors.ad904c74.js"
+  },
+  {
+    "revision": "204bfeb33c505f02ee0476b3ea6e1cde",
+    "url": "/web-font-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/web-font-test/robots.txt"
+  }
+]);
